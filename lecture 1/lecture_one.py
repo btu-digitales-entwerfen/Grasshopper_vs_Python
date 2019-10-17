f7983bdf6e92d66da7f3a35f1cc9ae3004070407
Anatolii Kotov <anatolii.kotov@b-tu.de>
2019-10-17 17:18:06 +0200

    lecture one

---
+++ lecture 1/lecture_one.py
@@ -0,0 +1,27 @@
+print("hello world")
+
+temporary = 1
+temporary2 = 1.6
+result = temporary + temporary2
+print(result)
+
+text_var = "some text"
+num_var = 65465
+print(text_var,num_var)
+
+number = 123.6
+text_var += ' '+str(number)
+text_var += ' number'
+print(text_var)
+
+colours = ['yellow','red','blue']
+index = 2
+current_colour = 'Our favourite is '
+current_colour += colours[index]
+print(current_colour)
+
+
+temp = 0
+temp = temp + 5 #this does some stuff
+temp += 5 #this does shortly
+print(temp)
